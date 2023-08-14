9c18d88d40fe8f8c547cf046c9346868499c4ef8
donzy <donzy@LAPTOP-1JMOM7PK.localdomain>
2023-08-14 21:41:03 +0200

    third feature

--- script.py
+++ script.py
@@ -1,11 +1,14 @@
 
 
 message = "this is an example program"
-print(message)
 
 
 def second_feature():
     return "this is a second feature"
 
+def thrid_feature():
+    return "this is a third feature"
 
+print(message)
 print(second_feature())
+print(thrid_feature())
